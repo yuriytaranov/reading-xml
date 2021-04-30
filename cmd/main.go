f241a043279ae612d459bba2c13dce65160be169
Yuriy Taranov <ig.seldom@gmail.com>
2021-04-30 09:55:15 +0300

    Adding structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"os"
 )
+
+// our struct which contains the complete
+// array of all Users in the file
+type Users struct {
+	XMLName xml.Name `xml:"users"`
+	Users   []User   `xml:"user"`
+}
+
+// the user struct, this contains our
+// Type attribute, our user's name and
+// a social struct which will contain all
+// our social links
+type User struct {
+	XMLName xml.Name `xml:"user"`
+	Type    string   `xml:"type,attr"`
+	Name    string   `xml:"name"`
+	Social  Social   `xml:"social"`
+}
+
+// a simple struct which contains all our
+// social links
+type Social struct {
+	XMLName  xml.Name `xml:"social"`
+	Facebook string   `xml:"facebook"`
+	Twitter  string   `xml:"twitter"`
+	Youtube  string   `xml:"youtube"`
+}
 
 func main() {
 	// Open our xmlFile
