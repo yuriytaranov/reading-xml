962f34acb9359301be7f18e4689951a6b017d8aa
Yuriy Taranov <ig.seldom@gmail.com>
2021-04-30 11:04:23 +0300

    Reading xml file

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -44,4 +45,22 @@ func main() {
 	fmt.Println("Successfully Opened users.xml")
 	//defer the closing of our xmlFile so that we can parse it later on
 	defer xmlFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	// we initialize our Users array
+	var users Users
+	// we unmarshal our byteArray which contains our
+	// xmlFiles content int 'users' which we defined above
+	xml.Unmarshal(byteValue, &users)
+
+	// we iterate through every user within our users array and
+	// print out the user Type, their name, and their facebook url
+	// as just an example
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+	}
 }
